142e55a65b99c8a35b0212ea05d36a250d6f96dc
OmarAraby <omararaby789@gmail.com>
2025-09-15 23:48:03 +0300

    add UnitOfWork interface

---
+++ Estore.Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace Estore.Domain.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUserRepository UserRepository { get; }
+        IProductRepository ProductRepository { get; }
+        IProductImageRepository ProductImageRepository { get; }
+        IRefreshTokenRepository RefreshTokenRepository { get; }
+        Task<int> SaveChangesAsync();
+    }
+}
